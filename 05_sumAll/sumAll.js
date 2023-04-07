02522d15d1431b6d6b588abb6787b13d5c682a84
yousef <yousefsameh24@gmail.com>
2023-04-07 14:56:17 +0200

    solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'ERROR';
+  }
+  if(a < 0 || b < 0) {
+    return 'ERROR';
+  }
+  let sum = 0;
+  if(a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  else{
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    }
+  return sum;
 };
 
 // Do not edit below this line
