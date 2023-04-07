360c6ea0d2fe3bb89b7b3af23e982b0f7c00ad42
yousef <yousefsameh24@gmail.com>
2023-04-07 15:09:55 +0200

    solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let ans = (degrees - 32) * 5 / 9;
+  let res = ans.toFixed(1);
+  let result = parseFloat(res);
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let res = ((degrees * 9 / 5) + 32).toFixed(1)
+  let result = parseFloat(res);
+  return result
 };
 
 // Do not edit below this line
