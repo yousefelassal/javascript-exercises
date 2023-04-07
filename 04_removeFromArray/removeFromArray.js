a1215b29c3b33279b3eadcd8fb4c1aaecd85b477
yousef <yousefsameh24@gmail.com>
2023-04-07 14:42:31 +0200

    solve 03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!values.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr
 
 };
 
