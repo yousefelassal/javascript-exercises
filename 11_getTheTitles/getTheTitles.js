763a414f4da6a26078b54637a6f18f635296ee58
yousef <yousefsameh24@gmail.com>
2023-04-19 12:30:22 +0200

    11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(x) {
+    let titles = [];
+    for (let i = 0; i < x.length; i++) {
+        titles.push(x[i].title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
