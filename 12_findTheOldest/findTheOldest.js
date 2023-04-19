675efb053055e0f2d0d8d7f5c33ae017c3d5d553
yousef <yousefsameh24@gmail.com>
2023-04-19 12:34:28 +0200

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let oldestAge = 0;
+    let currentAge = 0;
+    let currentYear = new Date().getFullYear();
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath) {
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            currentAge = currentYear - people[i].yearOfBirth;
+        }
+        if (currentAge > oldestAge) {
+            oldestAge = currentAge;
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
