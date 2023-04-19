2286c07b9da76aedd57a8634d5a9e3e3195a6f97
yousef <yousefsameh24@gmail.com>
2023-04-19 12:24:24 +0200

    09
    regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+    let y = x.toLowerCase().replace(/[^a-z]/g, "");
+    let z = y.split("").reverse().join("");
+    return y === z;
 };
 
 // Do not edit below this line
