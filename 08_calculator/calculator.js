7820202f15cdee34cfedefdbe15251c9d3aafa18
yousef <yousefsameh24@gmail.com>
2023-04-19 12:14:37 +0200

    08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
 const sum = function() {
-	
+  let sum = 0;
+  for (let i = 0; i < arguments[0].length; i++) {
+    sum += arguments[0][i];
+  }
+  return sum;
 };
 
 const multiply = function() {
-
+  let product = 1;
+  for (let i = 0; i < arguments[0].length; i++) {
+    product *= arguments[0][i];
+  }
+  return product;
 };
 
 const power = function() {
-	
+  let power = 1;
+  for (let i = 0; i < arguments[1]; i++) {
+    power *= arguments[0];
+  }
+  return power;
 };
 
 const factorial = function() {
-	
+  let factorial = 1;
+  for (let i = 1; i <= arguments[0]; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
